8d1a5880943fbfb7d48dbf4b18adeea17d2f9c5a
Dominik Drozd <dominik_drozd@vp.pl>
2019-12-19 13:58:46 +0100

    add user menu

--- PersonalBudget.cpp
+++ PersonalBudget.cpp
@@ -28,6 +28,48 @@ void PersonalBudget::registrationMenu() {
     }
 }
 
+void PersonalBudget::userMenu() {
+    system("cls");
+    cout << "    >>> MENU UZYTKOWNIKA <<<     " << endl;
+    cout << "---------------------------------" << endl;
+    cout << "1. Dodaj przychod" << endl;
+    cout << "2. Dodaj wydatek" << endl;
+    cout << "3. Bilans z biezacego miesiaca" << endl;
+    cout << "4. Bilans z poprzedniego miesiaca" << endl;
+    cout << "5. Bilans z wybranego okresu" << endl;
+    cout << "---------------------------------" << endl;
+    cout << "6. Zmien haslo" << endl;
+    cout << "7. Wyloguj sie" << endl;
+    cout << "---------------------------------" << endl;
+    cout << "Twoj wybor: ";
+    switch (AuxiliaryMethods::loadSign()) {
+    case '1':
+     //   addIncome();
+        break;
+    case '2':
+     //   addExpense();
+        break;
+    case '3':
+     //   showCurrentMonthBalance();
+        break;
+    case '4':
+     //   showPreviousMonthBalance();
+        break;
+    case '5':
+    //    showSelectedPeriodBalance();
+        break;
+    case '6':
+    //    changingPasswordOfLoggedUser();
+        break;
+    case '7':
+    //    userLogOut();
+        break;
+    default:
+        cout << endl << "Nie ma takiej opcji w menu." << endl << endl;
+        system("pause");
+        break;
+    }
+}
 
 void PersonalBudget::userRegistration()
 {
